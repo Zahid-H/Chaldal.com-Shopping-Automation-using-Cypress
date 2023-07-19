dd0559b4ddd47800888b0677c218d135eb3ea5e0
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-07-19 22:11:06 +0600

    clicking on the catagories Home ceaning suplies and select a product

--- cypress/e2e/Chaldal-shopping-automation/Chaldal.cy.js
+++ cypress/e2e/Chaldal-shopping-automation/Chaldal.cy.js
@@ -7,11 +7,11 @@ describe('Online Chaldal automation shopping', () => {
 
     it('clicking on the catagories soft drinks and select a product', () => {
         //Selecting beverages section
-        cy.get('body > div:nth-child(2) > div:nth-child(1) > div:nth-child(8) > section:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > section:nth-child(1) > div:nth-child(2) > a:nth-child(4) > div:nth-child(1) > div:nth-child(1)').click()
+        cy.get('[href="cleaning"] > .categoryBox > .categoryName').click()
         //Seleting soft Drinks 
         cy.get('body > div:nth-child(2) > div:nth-child(1) > div:nth-child(8) > section:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > section:nth-child(2) > div:nth-child(2) > div:nth-child(1) > a:nth-child(2) > div:nth-child(1) > div:nth-child(2)').click()
         // Adding the  item on the list
-        cy.contains('Coca-Cola').click()
+        cy.contains('Sprite').click()
         //check the shping card
         cy.wait(2000)
         cy.get("div[class='total']").click()
@@ -57,6 +57,21 @@ describe('Online Chaldal automation shopping', () => {
         cy.get('body > div:nth-child(2) > div:nth-child(1) > div:nth-child(8) > section:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > section:nth-child(2) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > div:nth-child(1) > div:nth-child(2)').click()
         // Adding the item on the list
         cy.contains('Radhuni Chilli (Morich) Powder').click()
+        //check the shping card
+        cy.wait(2000)
+        cy.get("div[class='total']").click()
+        cy.wait(2000)
+
+    })
+    it('clicking on the catagories Home ceaning suplies and select a product', () => {
+        //Selecting Home cleaning suplies section
+
+        cy.get('[href="cleaning"] > .categoryBox > .categoryName').click()
+
+        //Seleting Toilet cleaners
+        cy.get('body > div:nth-child(2) > div:nth-child(1) > div:nth-child(8) > section:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > section:nth-child(2) > div:nth-child(2) > div:nth-child(1) > a:nth-child(3) > div:nth-child(1) > div:nth-child(2)').click()
+        // Adding the item on the list
+        cy.contains('Harpic Liquid Toilet Cleaner 750 ml & Lizol Citrus Floor Cleaner 500 ml').click()
         //check the shping card
         cy.wait(2000)
         cy.get("div[class='total']").click()
