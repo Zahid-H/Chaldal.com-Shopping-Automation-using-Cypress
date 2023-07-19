36fadc71dc16b95c131ff3b7b88cce3896358673
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-07-19 20:29:25 +0600

    project file being created

---
+++ cypress/e2e/Chaldal-shopping-automation/Chaldal.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('Online Chaldal automation shopping', () => {
+    beforeEach(() => {
+        cy.visit('https://chaldal.com/')
+    });
+    it('', () => {
+        
+    });
+});
