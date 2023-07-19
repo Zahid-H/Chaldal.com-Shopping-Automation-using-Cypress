a9325816640a5bffa35c198cf79484db83726a67
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-07-19 21:58:22 +0600

    clicking on the catagories Cooking and select a product automation

--- cypress/e2e/Chaldal-shopping-automation/Chaldal.cy.js
+++ cypress/e2e/Chaldal-shopping-automation/Chaldal.cy.js
@@ -5,12 +5,12 @@ describe('Online Chaldal automation shopping', () => {
         cy.visit('https://chaldal.com/')
     })
 
-    it('clicking on the catagories soft drinks', () => {
+    it('clicking on the catagories soft drinks and select a product', () => {
         //Selecting beverages section
         cy.get('body > div:nth-child(2) > div:nth-child(1) > div:nth-child(8) > section:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > section:nth-child(1) > div:nth-child(2) > a:nth-child(4) > div:nth-child(1) > div:nth-child(1)').click()
         //Seleting soft Drinks 
         cy.get('body > div:nth-child(2) > div:nth-child(1) > div:nth-child(8) > section:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > section:nth-child(2) > div:nth-child(2) > div:nth-child(1) > a:nth-child(2) > div:nth-child(1) > div:nth-child(2)').click()
-        // Adding the first item on the list
+        // Adding the  item on the list
         cy.contains('Coca-Cola').click()
         //check the shping card
         cy.wait(2000)
@@ -18,30 +18,45 @@ describe('Online Chaldal automation shopping', () => {
         cy.wait(2000)
 
     })
-    it('clicking on the catagories Fruits and vegetables ', () => {
+    it('clicking on the catagories Fruits and vegetables select a product', () => {
         //Selecting fruits and vegtables section
 
         cy.get('[href="fruits-vegetables"] > .categoryBox > .categoryName').click()
 
         //Seleting Fruits 
         cy.get('body > div:nth-child(2) > div:nth-child(1) > div:nth-child(8) > section:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > section:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(2) > div:nth-child(1) > div:nth-child(2)').click()
-        // Adding the first item on the list
-        cy.contains('Shagor Kola (Banana Sagor)').click()
+        // Adding the  item on the list
+        cy.contains('Shagor Kola').click()
         //check the shping card
         cy.wait(2000)
         cy.get("div[class='total']").click()
         cy.wait(2000)
 
     })
-    it('clicking on the catagories Meat and fish ', () => {
-        //Selecting fruits and vegtables section
+    it('clicking on the catagories Meat and fish select a product', () => {
+        //Selecting Meat and fish section
 
         cy.get('[href="meat-fish"] > .categoryBox > .categoryName').click()
 
-        //Seleting Fruits 
+        //Seleting chicken
         cy.get('body > div:nth-child(2) > div:nth-child(1) > div:nth-child(8) > section:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > section:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > div:nth-child(1) > div:nth-child(2)').click()
-        // Adding the first item on the list
+        // Adding the  item on the list
         cy.contains('Chaldal Premium Roast Chicken ± 50 gm').click()
+        //check the shping card
+        cy.wait(2000)
+        cy.get("div[class='total']").click()
+        cy.wait(2000)
+
+    })
+    it('clicking on the catagories Cooking and select a product', () => {
+        //Selecting spices section
+
+        cy.get('[href="cooking"] > .categoryBox').click()
+
+        //Seleting spices
+        cy.get('body > div:nth-child(2) > div:nth-child(1) > div:nth-child(8) > section:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > section:nth-child(2) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > div:nth-child(1) > div:nth-child(2)').click()
+        // Adding the item on the list
+        cy.contains('Radhuni Chilli (Morich) Powder').click()
         //check the shping card
         cy.wait(2000)
         cy.get("div[class='total']").click()
